2ced94b2692e84c9d806aa96a0929298f9b65ab1
Hiroshi Sugawara <5000164@users.noreply.github.com>
2020-02-28 17:00:58 +0100

    chore: preparing to use images

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,8 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 600,
+              linkImagesToOriginal: false,
+              showCaptions: true,
             },
           },
           {
